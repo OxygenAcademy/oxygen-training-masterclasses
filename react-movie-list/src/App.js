17ee80d1f711f70fee492d36d0d1c557f14d2b69
DevIdCarvajal <davidcarvajalg@gmail.com>
2023-05-09 15:49:02 +0200

    React fundamentals fixes

--- react-movie-list/src/App.js
+++ react-movie-list/src/App.js
@@ -1,61 +1,33 @@
 import './App.css';
 import Movie from "./components/Movie";
 import { useState, useEffect } from 'react';
-import UserContext from "./contexts/UserContext";
 
-const initialCategories = [
+const data = [
   {
-    id: 1,
-    name: "Horror"
+    title: "La guerra de las falacias",
+    year: "1978"
   },
   {
-    id: 2,
-    name: "Romance"
+    title: "El señor de los gramillos",
+    year: "2001"
   }
-];
+]
 
-const categoryReducer = (state, action) => {
-  switch (action.type) {
-
-    case "GET_CATEGORIES":
-      return state;
-
-    case "GET_CATEGORY":
-      return state.filter(category => category.id === action.payload);
-
-    case "POST_CATEGORY":
-      return [ ...state, action.payload ];
-      
-    default:
-      return state;
-  }
-};
-
-function App() {
+const App = () => {
+  const [title, setTitle] = useState("Mi super aplicación");
   const [user, setUser] = useState({});
   const [movies, setMovies] = useState([]);
   const [totalMovies, setTotalMovies] = useState(0);
 
-  const [categories, dispatchCategories] = useReducer(categoryReducer, initialCategories);
-
   // Mounting
   useEffect(() => {
     setUser({
-      name: "david",
-      age: 23
+      name: "Goku",
+      level: 9000
     });
 
     let timer = setTimeout(() => {
-      setMovies([
-        {
-          title: "La guerra de las falacias",
-          year: "1978"
-        },
-        {
-          title: "El señor de los gramillos",
-          year: "2001"
-        }
-      ]);
+      setMovies(data);
     }, 2000);
 
     // Cleanup action
@@ -65,35 +37,30 @@ function App() {
   useEffect(() => {
     setTotalMovies(movies.length)
   }, [movies]);
-  
-  const showCategories = () => {
-    dispatchCategories({ type: "GET_CATEGORIES" })
-  }
 
   return (
-    <UserContext.Provider value={user}>
-      <div className="App">
-        {
-          movies.map((movie, index) => {
-            return (
-              <Movie title={movie.title} year={movie.year} key={index} />
-            )
-          })
-        }
-        <h2>{totalMovies}</h2>
+    <div className="App">
+      <h1>{title}</h1>
 
-        <h2>Categorías</h2>
-        {
-          categories.map((cat) => {
-            return (
-              <p>{cat.name}</p>
-            )
-          })
-        }
-
-        <button onClick={showCategories}>Ver categorías</button>
+      <h2>Datos de usuario</h2>
+      <div>
+        <p>Usuario: {user.name}</p>
+        <p>Nivel: {user.level}</p>
       </div>
-    </UserContext.Provider>
+
+      <h2>Mis películas</h2>
+      <div>
+      {
+        movies.map((movie, index) => {
+          return (
+            <Movie title={movie.title} year={movie.year} key={index} />
+          )
+        })
+      }
+      </div>
+      
+      <p>Total: {totalMovies}</p>
+    </div>
   );
 }
 
